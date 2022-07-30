642d659a317c784353a307cd1cf54813170103ca
Noel Light-Hilary <noel@noellh.com>
2022-07-30 09:31:12 +0100

    ID-3 – fix version

--- src/Application/Actions/Action.php
+++ src/Application/Actions/Action.php
@@ -14,7 +14,7 @@ use Slim\Exception\HttpNotFoundException;
 use Symfony\Component\Validator\ConstraintViolation;
 
 /**
- * @OA\Info(title="Big Give Identity service", version="1"),
+ * @OA\Info(title="Big Give Identity service", version="0.0.1"),
  * @OA\Server(
  *     description="Staging",
  *     url="https://identity-staging.thebiggivetest.org.uk",
