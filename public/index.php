8dc91cb766f6d8736ab07a67369ad2c9eb939f64
Noel Light-Hilary <noel@noellh.com>
2022-10-14 18:07:58 +0100

    Fix `HttpErrorHandler` having no logger

--- public/index.php
+++ public/index.php
@@ -7,6 +7,7 @@ use BigGive\Identity\Application\Handlers\ShutdownHandler;
 use BigGive\Identity\Application\ResponseEmitter\ResponseEmitter;
 use BigGive\Identity\Application\Settings\SettingsInterface;
 use DI\ContainerBuilder;
+use Psr\Log\LoggerInterface;
 use Slim\Factory\AppFactory;
 use Slim\Factory\ServerRequestCreatorFactory;
 
@@ -41,7 +42,12 @@ $request = $serverRequestCreator->createServerRequestFromGlobals();
 
 // Create Error Handler
 $responseFactory = $app->getResponseFactory();
-$errorHandler = new HttpErrorHandler($callableResolver, $responseFactory);
+
+$errorHandler = new HttpErrorHandler(
+    $callableResolver,
+    $responseFactory,
+    $app->getContainer()->get(LoggerInterface::class),
+);
 
 // Create Shutdown Handler
 $shutdownHandler = new ShutdownHandler($request, $errorHandler, $displayErrorDetails);
