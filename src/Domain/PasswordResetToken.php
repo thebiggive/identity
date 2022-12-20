8f5d8c87f2103d9fc32bae2b94f3a5a2a6a3361c
Noel Light-Hilary <noel@noellh.com>
2022-12-20 16:59:05 +0000

    Datetime tweaks

--- src/Domain/PasswordResetToken.php
+++ src/Domain/PasswordResetToken.php
@@ -47,8 +47,10 @@ class PasswordResetToken
     {
         $this->person = $person;
         $this->secret = $secret;
-        $this->created_at = new \DateTime();
-        $this->updated_at = clone $this->created_at;
+
+        $now = new \DateTimeImmutable();
+        $this->created_at = $now;
+        $this->updated_at = $now;
     }
 
     public static function fromBase58(Person $person, string $base58Secret): self
