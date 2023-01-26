4e7483f76dbe4aaa1bdf393cf0e073684289bdd0
Barney Laurance <barney@thebiggive.org.uk>
2023-01-26 15:18:55 +0000

    ID-35: Reduce all JWT's lifetime to 8 hours

--- tests/Application/Auth/TokenTest.php
+++ tests/Application/Auth/TokenTest.php
@@ -6,10 +6,17 @@ namespace BigGive\Identity\Tests\Application\Auth;
 
 use BigGive\Identity\Application\Auth\Token;
 use BigGive\Identity\Tests\TestCase;
+use Firebase\JWT\JWT;
 use Psr\Log\NullLogger;
 
 class TokenTest extends TestCase
 {
+    public function tearDown(): void
+    {
+        JWT::$timestamp = null;
+        parent::tearDown();
+    }
+
     public function testCreateReturnsValidLookingToken(): void
     {
         $token = Token::create('somePersonId', true, 'cus_aaaaaaaaaaaa11');
@@ -43,5 +50,22 @@ class TokenTest extends TestCase
         $token = Token::create('somePersonId', false, 'cus_aaaaaaaaaaaa11');
 
         $this->assertFalse(Token::check('somePersonId', true, $token, new NullLogger()));
+    }
+
+    public function testTokenValidFor7Hours59(): void
+    {
+        $token = Token::create('somePersonId', true, 'cus_aaaaaaaaaaaa11');
+
+        JWT::$timestamp = (new \DateTimeImmutable('+ 7 hours 59 minutes'))->getTimestamp();
+
+        $this->assertTrue(Token::check('somePersonId', true, $token, new NullLogger()));
+    }
+    public function testTokenExpiresInEightHours(): void
+    {
+        $str = Token::create('somePersonId', true, 'cus_aaaaaaaaaaaa11');
+
+        JWT::$timestamp = (new \DateTimeImmutable('+ 8 hours'))->getTimestamp();
+
+        $this->assertFalse(Token::check('somePersonId', true, $str, new NullLogger()));
     }
 }
