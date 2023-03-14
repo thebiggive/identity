19ee2ad9eaa43d0d6158b624631581635eb7e38b
Barney Laurance <barney@thebiggive.org.uk>
2023-03-14 12:11:24 +0000

    ID-32: Add integration test for PersonRepository with real DB

---
+++ integrationTests/PersonRepositoryTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace BigGive\Identity\IntegrationTests;
+
+use BigGive\Identity\Domain\Person;
+use BigGive\Identity\Repository\PersonRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Uid\Uuid;
+
+class PersonRepositoryTest extends IntegrationTest
+{
+    public function testItDoesNotFindPersonWhoDoesNotExist(): void
+    {
+        $sut = $this->getService(PersonRepository::class);
+
+        $found = $sut->find(Uuid::v4());
+
+        $this->assertNull($found);
+    }
+
+    public function testItPersistsAndFindsPerson(): void
+    {
+        $sut = $this->getService(PersonRepository::class);
+        $em = $this->getService(EntityManagerInterface::class);
+
+        $person = new Person();
+        // use a unique email address every time to avoid conflict with data already in DB.
+        $email = "someemail" . Uuid::v4() . "@example.com";
+        $person->email_address = $email;
+        $person->first_name = "Fred";
+        $person->raw_password = 'password';
+
+        $sut->persist($person);
+        $em->clear();
+
+        $found = $sut->findPasswordEnabledPersonByEmailAddress($email);
+
+        $this->assertNotSame($person, $found);
+        $this->assertNotNull($found);
+        $this->assertEquals($email, $found->email_address);
+        $this->assertEquals("Fred", $found->first_name);
+    }
+}
