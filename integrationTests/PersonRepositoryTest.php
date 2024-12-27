bbe7c8c991dcbb30cf05d93b45d0f281d30d4262
Noel Light-Hilary <noel@noellh.com>
2024-12-27 15:43:15 +0000

    MAT-379 – cover new bus dispatch in Person persistence integration test

--- integrationTests/PersonRepositoryTest.php
+++ integrationTests/PersonRepositoryTest.php
@@ -5,6 +5,12 @@ namespace BigGive\Identity\IntegrationTests;
 use BigGive\Identity\Domain\Person;
 use BigGive\Identity\Repository\PersonRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use MatchBot\Application\Messenger\CharityUpdated;
+use MatchBot\Application\Messenger\Handler\CharityUpdatedHandler;
+use MatchBot\Tests\TestCase;
+use Prophecy\Argument;
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\RoutableMessageBus;
 use Symfony\Component\Uid\Uuid;
 
 class PersonRepositoryTest extends IntegrationTest
@@ -23,14 +29,36 @@ class PersonRepositoryTest extends IntegrationTest
         $sut = $this->getService(PersonRepository::class);
         $em = $this->getService(EntityManagerInterface::class);
 
+        $uuid = Uuid::v4();
         $person = new Person();
+        $person->setId($uuid);
         // use a unique email address every time to avoid conflict with data already in DB.
-        $email = "someemail" . Uuid::v4() . "@example.com";
+        $email = "someemail.$uuid@example.com";
         $person->email_address = $email;
         $person->first_name = "Fred";
         $person->last_name = "Bloggs";
         $person->raw_password = 'password';
         $person->stripe_customer_id = 'cus_1234567890';
+        $person->hashPassword();
+
+        $busProphecy = $this->prophesize(RoutableMessageBus::class);
+        $busProphecy->dispatch(Argument::type(Envelope::class))
+            ->will(
+                /**
+                 * @param array{0: Envelope} $args
+                 */
+                function (array $args) use ($person): Envelope {
+                    $envelope = $args[0];
+                    /** @var \Messages\Person $message */
+                    $message = $envelope->getMessage();
+                    \PHPUnit\Framework\TestCase::assertInstanceOf(\Messages\Person::class, $message);
+                    \PHPUnit\Framework\TestCase::assertSame($person->stripe_customer_id, $message->stripe_customer_id);
+
+                    return $envelope;
+                }
+            )
+            ->shouldBeCalledOnce();
+        $sut->setBus($busProphecy->reveal());
 
         $sut->persist($person);
         $em->clear();
