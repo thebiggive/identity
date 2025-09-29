3d2f2bc238bc1d7f4287ffd8576a1eee98fbf7ce
Barney Laurance <barney@thebiggive.org.uk>
2025-09-29 19:07:07 +0100

    BG2-2973: Delete unwanted donor account

---
+++ src/Migrations/Version20250929180517.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BigGive\Identity\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250929180517 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Delete unwanted donor account';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql("DELETE FROM Person WHERE stripe_customer_id = 'cus_SyVojkUam4cxv5' LIMIT 1");
+    }
+
+    public function down(Schema $schema): void
+    {
+        // no going back
+    }
+}
