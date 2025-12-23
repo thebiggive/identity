1be978e0c6ac5726005f4493a799e4b3f65c5929
Barney Laurance <barney@thebiggive.org.uk>
2025-12-23 12:03:38 +0000

    BG2-3031: Add action to delete a person record if requested by that logged in person
    Still need to make sure the deletion gets automatically carried over to
    matchbot as well.

---
+++ integrationTests/DeletePersonTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace BigGive\Identity\IntegrationTests;
+
+use BigGive\Identity\Application\Auth\Token;
+use BigGive\Identity\Domain\Person;
+use BigGive\Identity\Repository\PersonRepository;
+use GuzzleHttp\Psr7\ServerRequest;
+use Prophecy\Argument;
+use Slim\Exception\HttpNotFoundException;
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\RoutableMessageBus;
+use Symfony\Component\Uid\Uuid;
+
+class DeletePersonTest extends IntegrationTest
+{
+    public function testItDeletesAPersonRecord(): void
+    {
+        $uuid = $this->addPersonToToDB(
+            emailAddress: "someemail" . Uuid::v4() . "@example.com",
+            password: 'topsecret',
+        )->toRfc4122();
+
+        $this->getApp()->handle(new ServerRequest(
+            method: 'DELETE',
+            uri: "/v1/people/{$uuid}",
+            headers: ['x-tbg-auth' => Token::create(new \DateTimeImmutable(), $uuid, true, '')],
+            body: '{"password": "topsecret"}',
+        ));
+
+        try {
+            $this->getApp()->handle(new ServerRequest(
+                method: 'GET',
+                uri: "/v1/people/{$uuid}",
+                headers: ['x-tbg-auth' => Token::create(new \DateTimeImmutable(), $uuid, true, '')],
+            ));
+
+            $this->fail('Person should not be found after deletion');
+        } catch (HttpNotFoundException $exception) {
+            $this->assertSame($exception->getMessage(), 'Person not found');
+        }
+    }
+}
