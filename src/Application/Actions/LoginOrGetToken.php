298f0ea16c20d2b4edd66786ae08b9eccf0f25d9
Barney Laurance <barney@thebiggive.org.uk>
2026-08-10 12:11:21 +0100

    DON-1195: Remove unecassary docblock

--- src/Application/Actions/LoginOrGetToken.php
+++ src/Application/Actions/LoginOrGetToken.php
@@ -45,8 +45,6 @@ class LoginOrGetToken extends Action
     }
 
     /**
-     * @param array $args
-     * @return Response
      * @throws HttpBadRequestException
      */
     protected function action(Request $request, array $args): Response
